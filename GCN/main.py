cf9bec7dbad2486de4df78a39eccb1b2924d2ca1
Cow-Kite <bbsr0819@gmail.com>
2024-02-07 15:10:19 +0900

    GCN/main.py update

--- GCN/main.py
+++ GCN/main.py
@@ -20,7 +20,8 @@ class GCN(torch.nn.Module):
         output = self.conv2(x, edge_index)
         return F.log_softmax(output, dim=1)
     
-def train_node_classifier(model, graph, optimizer, criterion, n_epochs=100):
+def train_node_classifier(model, graph, optimizer, criterion, n_epochs=200):
+    train_start = time.time()
     for epoch in range(1, n_epochs + 1):
         model.train() 
         optimizer.zero_grad()
@@ -34,6 +35,8 @@ def train_node_classifier(model, graph, optimizer, criterion, n_epochs=100):
 
         if epoch % 10 == 0:
             print(f'Epoch: {epoch:03d}, Train Loss: {loss:.3f}, Val Acc: {acc:.3f}')
+    train_end = time.time()
+    print("training time: %.3f초" %(train_end-train_start))
     return model
 
 def eval_node_classifier(model, graph, mask):
@@ -43,14 +46,14 @@ def eval_node_classifier(model, graph, mask):
     acc = int(correct) / int(mask.sum())
     return acc
 
-start_time = time.time()
-
 device = "cpu"
 # Data
-dataset = Planetoid(root='./', name='Cora')
+dataset = Planetoid(root='./planetoid', name='Cora')
 graph = dataset[0]
 split = T.RandomNodeSplit(num_val=0.1, num_test=0.2)
 graph = split(graph)
+
+time.sleep(20)
 
 gcn = GCN().to(device)
 optimizer_gcn = torch.optim.Adam(gcn.parameters(), lr=0.01, weight_decay=5e-4)
@@ -59,7 +62,6 @@ gcn = train_node_classifier(gcn, graph, optimizer_gcn, criterion)
 
 test_acc = eval_node_classifier(gcn, graph, graph.test_mask)
 print(f'Test Acc: {test_acc:.3f}')
-end_time = time.time()
-print("총 소요 시간: %.3f초" %(end_time - start_time))
 
-time.sleep(500)
+
+
